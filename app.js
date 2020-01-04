fca8c577ca2b903bd88f20ba71b5318f468cf7e6
m9dnts8608 <leah8608@gmail.com>
2020-01-04 20:10:55 +0900

    #6.5 MongoStore

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
+import mongoose from "mongoose"
+import session from "express-session";
+import MongoStore from "connect-mongo"
 import userRouter from "./Routers/userRouter";
 import videoRouter from "./Routers/videoRouter";
 import globalRouter from "./Routers/globalRouter";
 import routes from "./routes";
 import { localsMiddleware } from "./middlewares";
 
+import "./passport";
+
 const app = express();
 
 //function
+const CookieStore = MongoStore(session)
 
 //global middleware functinon
 app.use(helmet());
@@ -22,6 +29,16 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+	session({
+		secret: process.env.COOKIE_SECRET,
+		resave: true,
+		saveUninitialized: false
+		store: new CookieStore({ mongooseConnection: mongoose.connection})
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(localsMiddleware);
 //Routers
---
+++ models/User.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import passportLocalMongoose from "passport-local-mongoose";
+
+const UserSchema = new mongoose.Schema({
+	name: String,
+	email: String,
+	avatarUrl: String,
+	facebookId: Number,
+	githubId: Number,
+	kakaoId: Number,
+	yahooId: Number,
+	lineId: Number,
+	instagramId: Number
+});
+
+UserSchema.plugin(passportLocalMongoose, { usernameField: "email" });
+
+const model = mongoose.model("User", UserSchema);
+
+export default model;
