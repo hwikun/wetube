b8ac496318f19da155570ce11d42e229c38fa846
m9dnts8608 <leah8608@gmail.com>
2020-01-13 01:16:47 +0900

    #10.4 api Adding a Comment

--- routes.js
+++ routes.js
@@ -29,6 +29,7 @@ const GITHUB_CALLBACK = "/auth/github/callback";
 
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment";
 
 // Object
 
@@ -75,7 +76,8 @@ const routes = {
     githubCallback: GITHUB_CALLBACK,
     me: ME,
     api: API,
-    registerView: REGISTER_VIEW
+    registerView: REGISTER_VIEW,
+    addComment: ADD_COMMENT
 };
 
 export default routes;
